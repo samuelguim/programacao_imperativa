5b2205635f48ebbc6fd34ab2c05c804a35085dfb
samuelguim <code.samuelg@gmail.com>
2024-02-01 00:48:08 -0300

    new file:   "estruturas_de_decis\303\243o/ex3-4" 	modified:   "estruturas_de_decis\303\243o/ex3-4.c"

--- estruturas_de_decisão/ex3-4.c
+++ estruturas_de_decisão/ex3-4.c
@@ -12,6 +12,36 @@ LIDO.
 
 #include <stdio.h>
 
-int main (){
-  
+int f_invert (int numero) {
+  int u, d, c, total;
+  u = numero % 10;
+  d = (numero / 10) % 10;
+  c = (numero / 100) % 100;
+  total = (u*100)+(d*10)+(c*1);
+  return total;
+}
+
+int main () {
+  int numero, invertido;
+  printf ("Digite um número inteiro positivo de 3 dígitos: ");
+  scanf ("%d", &numero);
+  if (numero >= 100 && numero <= 999) {
+    invertido = f_invert (numero);
+    printf ("Número gerado: %d\n", invertido);
+    if (invertido == numero) {
+      printf ("NÚMERO GERADO IGUAL AO NÚMERO LIDO\n");
+    }
+    else {
+      if (numero > invertido) {
+        printf ("NÚMERO LIDO MAIOR QUE NÚMERO GERADO\n");
+      }
+      else {
+        printf ("NÚMERO GERADO MAIOR QUE NÚMERO LIDO\n");
+      }
+    }
+  }
+  else {
+    printf ("O número não satisfaz os requisitos.\n");
+  }
+  return 0;
 }
