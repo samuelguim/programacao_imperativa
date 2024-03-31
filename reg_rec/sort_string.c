19c7d5975f0d13c253d42aa7f7f07aafd4c6bdde
samuelguim <code.samuelg@gmail.com>
2024-03-30 22:37:09 -0300

    modified:   reg_rec/sort_string 	modified:   reg_rec/sort_string.c

--- reg_rec/sort_string.c
+++ reg_rec/sort_string.c
@@ -2,11 +2,7 @@
 #include <string.h>
 
 int bbinaria (char chave[21], int inicio, int fim, char vetor[10][21]) {
-  printf ("%d", strcmp(chave, vetor[0]));
-  if (fim < inicio) {
-    return -1;
-  }
-  else {
+
     int meio = (inicio + fim)/2;
     if (strcmp(chave, vetor[meio])==0){
       return meio; }
@@ -15,9 +11,10 @@ int bbinaria (char chave[21], int inicio, int fim, char vetor[10][21]) {
         return bbinaria (chave, meio+1, fim, vetor);}
       else {
         return bbinaria (chave, inicio, meio-1, vetor); }}}
-}
+
 
 int main (){
+  printf ("%d", strcmp("DIRECAO", "FELICIDADE"));
   char vetor_palavras[10][21] = {"AMOR","ATENCAO","BELEZA","BELO","DIRECAO","FELICIDADE","FELIZ","MALETA","REVOLTA","SABIO"};
   char palavra[21];
   int posicao;
@@ -25,7 +22,7 @@ int main (){
   printf ("Qual palavra você deseja encontrar? ");
   scanf (" %[^\n]s", palavra);
   posicao = bbinaria (palavra,0,9,vetor_palavras);
-  printf ("\nPosição: %d\n", posicao);
+  printf ("Posição: %d\n", posicao);
   printf ("Resultado: %s\n", vetor_palavras[posicao]);
   return 0;
 }
